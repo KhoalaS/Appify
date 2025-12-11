960d9b1214f39d2500b9756550330ce9d25c2bd7
KhoalaS <khoalekappa@gmail.com>
2025-12-11 14:47:31 +0100

    add base64 encode js interface

--- embeds/env.d.ts
+++ embeds/env.d.ts
@@ -1,10 +1,11 @@
 interface AndroidBridge {
-    showToast(message: string): void;
-    saveFile(filename: string, content: string): void;
+  showToast(message: string): void
+  saveFile(filename: string, content: string): void
+  base64EncodeResponseToDataUrl(url: string): string
 }
 
 declare global {
-    const Android: AndroidBridge;
+  const Android: AndroidBridge
 }
 
-export {};
+export {}
